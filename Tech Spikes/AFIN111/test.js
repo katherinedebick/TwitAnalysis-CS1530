da069ddba3e1d8d82fb92833792313d37bbde34e
Alex Drizos <adrizosmpb@Alexanders-MBP.fios-router.home>
2018-09-11 09:54:22 -0400

    afinn111 tech spike complete

--- Tech Spikes/AFIN111/test.js
+++ Tech Spikes/AFIN111/test.js
@@ -2,10 +2,14 @@ let fs = require('fs');
 var afinnStr = fs.readFileSync('AFINN-111.txt', 'utf8');
 let afinnArr = parse_String(afinnStr);
 
-for (var n in afinnArr){
-  console.log(afinnArr[n].word + " : " + afinnArr[n].score);
-}
+let tweet = "Wow I can't believe BestBuy screwed me today.";
+console.log("Original Tweet: " + tweet);
+tweet = tweet.toLowerCase();
+let score = getScore(tweet);
+console.log("Total Score: " + score);
 
+
+//reads in afinn111 string which is tab/newline delimited, saves as JS object/dictionary
 function parse_String(data){
   let splitData = data.split("\n");
   var dict = [];
@@ -17,4 +21,20 @@ function parse_String(data){
     });
   }
   return dict;
+}
+
+//extremely basic test of looking at each word in example tweet and calculuating score
+function getScore(tweet){
+  var tempScore = 0;
+  let tweetSplit = tweet.split(" ");
+  for (var n in tweetSplit){
+    console.log(tweetSplit[n]);
+    for (var w in afinnArr){
+      if (tweetSplit[n] == afinnArr[w].word){
+        tempScore += parseInt(afinnArr[w].score);
+        console.log(afinnArr[w].word + " -> score: " + afinnArr[w].score);
+      }
+    }
+  }
+  return tempScore;
 }

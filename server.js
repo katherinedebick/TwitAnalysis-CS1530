cc47f2c0ea5a0157ec9f9a40b267c1b27efc57aa
katherinedebick <kld87@pitt.edu>
2018-11-06 12:52:27 -0500

    Asynchronous Callback Hell is "Fixed" but not clean... will work on tonight. Try it out!

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
+var async = require('async');;
 
 var path = require('path');
 
@@ -50,31 +51,26 @@ app.get('/', function(req, res) {
 
 app.post('/gettweets', function(req, res){
   var temp_query = new Query(req.body.search_word, req.body.sample_size, req.body.start_date, req.body.end_date);
-  process_query(temp_query);
-  res.render(path.join(__dirname+'/views/tweets.ejs'), {tweets: tweets});
-  //res.sendFile(path.join(__dirname + '/pages/graph.html'));
-});
-
-function process_query(query){
-  console.log(query.search_word)
-  console.log(query.sample_size)
-  var params = {
-    q: query.search_word,
-    count: query.sample_size
-  }
-  T.get('search/tweets', params, function(err, data, response){
-    //return data;
-    var raw_tweets = data.statuses;
-    for (var i=0; i<raw_tweets.length; i++){
-      tweets.push(String(raw_tweets[i].text))
+  tweets2 = [];
+  async.until(function(){
+    if(tweets2.length == temp_query.sample_size){
+      res.render(path.join(__dirname+'/views/tweets.ejs'), {tweets: tweets2});
     }
-  });
-  console.log(tweets)
-  for (var i=0; i<tweets.length; i++){
-    console.log("Tweet: "+tweets[i])
-  }
-  //return tweets;
-}
+    return tweets2.length == temp_query.sample_size;
+  }, function process_query(cb){
+    var params = {
+      q: temp_query.search_word,
+      count: temp_query.sample_size
+    }
+    T.get('search/tweets', params, function(err, data, response){
+      var raw_tweets = data.statuses;
+      for (var i=0; i<raw_tweets.length; i++){
+        tweets2.push(String(raw_tweets[i].text));
+      }
+      cb();
+    });
+  })
+});
 
 
 app.listen(process.env.PORT || 5000);

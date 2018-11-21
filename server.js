7d58ae3f787ae615e5cf978b47998d4bb148413e
Alex Drizos <adrizosmpb@Alexanders-MBP.fios-router.home>
2018-11-20 23:27:35 -0500

    not working

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var Twit = require('twit');
 var config = require('./config'); //use this instead of putting keys in the server file
 let fs = require('fs');
 
+
+
 var afinnStr = fs.readFileSync('AFINN-111.txt', 'utf8');
 let afinnArr = parse_String(afinnStr);
 //set up Twitter API connection
@@ -40,39 +42,104 @@ app.get('/', function(req, res) {
 
 app.post('/gettweets', function(req, res){
   var temp_query = new Query(req.body.search_word, req.body.sample_size, req.body.start_date, req.body.end_date);
-  tweets2 = [];
+  var masterObject = processQuery(temp_query); // object contains: list of raw twitter tweet objects (.data),
+                                               //list of tweet statuses as strings(.statusStrings)
+  var scores = scoreTweets(masterObject.statusStrings);
+  var tweets
+  renderPage(masterObject.statusStrings, res, scores);
+
+
+  //var tweetObjects = getTweetsHelper(req,res); //page renders from inside function
+});
+
+
+//***working function before changes***
+// app.post('/gettweets', function(req, res){
+//   var temp_query = new Query(req.body.search_word, req.body.sample_size, req.body.start_date, req.body.end_date);
+//   tweets2 = []; //just tweet string
+//
+//   //Begin async Block
+//   //Until the tweets in tweets2 match the requested sample size or greater, don't render the page
+//   async.until(function(){
+//     if(tweets2.length >= temp_query.sample_size){
+//       scores = scoreTweets(tweets2);
+//       renderPage(tweets2, res, scores);
+//       //res.render(path.join(__dirname+'/views/tweets.ejs'), {tweets: tweets2});
+//     }
+//     return tweets2.length >= temp_query.sample_size;
+//   }, function process_query(cb){
+//     var params = {
+//       q: temp_query.search_word,
+//       count: temp_query.sample_size
+//     }
+//     T.get('search/tweets', params, function(err, data, response){
+//       // console.log('data: ' + data.statuses[0].created_at);
+//       //console.log('data: ' + JSON.stringify(data));
+//       var raw_tweets = data.statuses;
+//       for (var i=0; i<raw_tweets.length; i++){
+//         tweets2.push(String(raw_tweets[i].text));
+//       }
+//       cb();
+//     });
+//   })
+// });
+
+function processQuery(temp_query){
+  var _masterObject = {};
+  var params = {
+    q: temp_query.search_word,
+    count: temp_query.sample_size
+  }
+  T.get('search/tweets', params, function(err, data, response){
+    // console.log('data: ' + data.statuses[0].created_at);
+    //console.log('data: ' + JSON.stringify(data));
+    _masterObject.data = data;
+    var raw_tweets = data.statuses;
+    for (var i=0; i<raw_tweets.length; i++){
+      tweetStatusList.push(String(raw_tweets[i].text));
+    }
+    _masterObject.statusStrings = tweetStatusList;
+  });
+  return _masterObject;
+}
+
+
+function getTweetsHelper(req, res) {
+  var temp_query = new Query(req.body.search_word, req.body.sample_size, req.body.start_date, req.body.end_date);
+  var tweetStatusList = []; //strings
 
   //Begin async Block
   //Until the tweets in tweets2 match the requested sample size or greater, don't render the page
   async.until(function(){
-    if(tweets2.length >= temp_query.sample_size){
-      scores = scoreTweets(tweets2);
-      renderPage(tweets2, res, scores);
+    if(tweetStatusList.length >= temp_query.sample_size){
+      scores = scoreTweets(tweetStatusList);
+      renderPage(tweetStatusList, res, scores);
       //res.render(path.join(__dirname+'/views/tweets.ejs'), {tweets: tweets2});
     }
-    return tweets2.length >= temp_query.sample_size;
+    return tweetStatusList.length >= temp_query.sample_size;
   }, function process_query(cb){
     var params = {
       q: temp_query.search_word,
       count: temp_query.sample_size
     }
     T.get('search/tweets', params, function(err, data, response){
+      // console.log('data: ' + data.statuses[0].created_at);
+      //console.log('data: ' + JSON.stringify(data));
       var raw_tweets = data.statuses;
       for (var i=0; i<raw_tweets.length; i++){
-        tweets2.push(String(raw_tweets[i].text));
+        tweetStatusList.push(String(raw_tweets[i].text));
       }
       cb();
     });
   })
-});
-
+}
 
 function scoreTweets(tweets){
   let scores = [];
   for (var t in tweets) {
     var score = getScore(tweets[t]);
     scores.push(score);
-    console.log("Tweet: " + tweets[t] + " Score: " + score);
+    //console.log("Tweet: " + tweets[t] + " Score: " + score);
   }
   return scores;
 }
